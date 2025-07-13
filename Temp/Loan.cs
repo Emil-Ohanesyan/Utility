bd65e11dbde292c1f08e0b98726faa170560b1ee
ohanesyan.e <ohanesyan.e@izbank.ir>
2025-07-13 13:45:50 +0330

    using class loan.

--- Temp/Loan.cs
+++ Temp/Loan.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
