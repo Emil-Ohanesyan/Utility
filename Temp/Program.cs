7d992d9ed35473284c8169d01f9e090f6daa33f0
emil <you@example.com>
2026-06-21 19:58:20 +0330

    emil changing Program.cs

--- Temp/Program.cs
+++ Temp/Program.cs
@@ -2,31 +2,3 @@
 using Temp;
 
 Console.WriteLine("Hello, World!");
-
-static void x()
-{
-    Console.WriteLine("Hello");
-}
-
-static void y()
-{
-    Console.WriteLine("Bye");
-}
-
-
-static int mul(int a, int b)
-{
-    var result = a * b;
-    return result;
-}
-
-static int div(int number1, int number2)
-{
-    var result = number1 / number2;
-    return result;
-}
-
-var loan = new Loan();
-loan.SayHello();
-
-Console.ReadLine();
